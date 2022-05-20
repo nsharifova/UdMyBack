7aebe50f12f50890ef84c3686a75397741342538
Elxan Quliyev <qelxan177@gmail.com>
2022-05-20 19:27:22 +0400

    20.05.2022

--- UmdyApi/Program.cs
+++ UmdyApi/Program.cs
@@ -12,7 +12,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(opt => opt.JsonSerializerOptions.WriteIndented = true);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -44,7 +45,6 @@ builder.Services.AddCors(options =>
                 .AllowAnyMethod()
                 .AllowAnyHeader()
                 .WithMethods("PUT", "DELETE", "GET");
-
         }
      );
 });
